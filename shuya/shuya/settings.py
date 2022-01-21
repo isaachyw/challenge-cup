39a2f59572cd8b72b6f52c8ad7a6bb4e89d1bda4
ithaca2020 <ithaca2020@sjtu.edu.cn>
2022-01-21 14:40:19 +0800

    add volunteer

--- shuya/shuya/settings.py
+++ shuya/shuya/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-1haeg)ohu3u#)m2lrc+-j4m@&g00&%y(zc1u-eh1y0c6dvw^rw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['42.192.204.229']
-
+ALLOWED_HOSTS = ['42.192.204.229', '127.0.0.1']
 
 # Application definition
 
@@ -70,7 +68,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'shuya.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -80,7 +77,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -100,7 +96,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -114,10 +109,9 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- shuya/shuyaplat/admin.py
+++ shuya/shuyaplat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models.volunteer.volunteer import Volunteer
+
 
 # Register your models here.
+
+admin.site.register(Volunteer)
